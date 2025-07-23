c37b513ba3d8fa7430b014c699ef67d14c67db67
manjurai22 <manjurai611@gmail.com>
2025-07-23 22:49:31 +0545

    add-edit-delete

--- contact_app/urls.py
+++ contact_app/urls.py
@@ -3,4 +3,7 @@ from contact_app import views
 
 urlpatterns=[
     path("",views.contact_list,name="contact-list"),
+    path("delete/<int:pk>/", views.delete_contact, name='delete-contact'),
+    path("add/", views.add_contact, name='add-contact'),
+    path("edit/<int:pk>/", views.edit_contact, name='edit-contact'),
 ]
--- contact_app/views.py
+++ contact_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from contact_app.models import Contact
+from django.http import HttpResponseRedirect
 
 def contact_list(request):
     contacts=Contact.objects.all()
@@ -8,3 +9,26 @@ def contact_list(request):
         "contact_list.html",
         {"contacts":contacts},
     )
+
+def delete_contact(request,pk):
+    contact=Contact.objects.get(pk=pk)
+    contact.delete()
+    return HttpResponseRedirect("/")
+
+def add_contact(request):
+    print(request.method,request.POST)
+    if request.method=="GET":
+        return render(request,"")
+    else:
+        Contact.objects.add(name=request.POST["name"])
+        return HttpResponseRedirect("/")
+    
+def edit_contact(request,pk):
+    if request.method=="GET":
+        contact=Contact.objects.get(pk=pk)
+        return render(request,"",{"contact":contact},)
+    else:
+        contact=Contact.objects.get(pk=pk)
+        contact.contact=request.POST["name"]
+        contact.save()
+        return HttpResponseRedirect("/")
